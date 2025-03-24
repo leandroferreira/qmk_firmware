f6ada98bdfa5a34ae70a7b25bfa3b1da8f739df8
Leandro Ferreira <34287+leandroferreira@users.noreply.github.com>
2025-03-24 11:13:13 -0400

    copy from markstos

---
+++ keyboards/crkbd/keymaps/lferreira/rules.mk
@@ -0,0 +1,23 @@
+# markstos: Attempt to enable Bootmagic
+#BOOTMAGIC_ENABLE = lite
+
+# markstos: enable media keys
+EXTRAKEY_ENABLE = yes
+
+# markstos: smaller file size, little down-side
+LINK_TIME_OPTIMIZATION_ENABLE = yes
+
+# markstos: for combos!
+COMBO_ENABLE = yes
+
+# markstos
+# Backlighting is not RGB underglow or RGB matrix
+BACKLIGHT_ENABLE = yes  # Enable keyboard backlight functionality
+
+# This is for RGB *underglow*
+# https://github.com/qmk/qmk_firmware/blob/master/docs/feature_rgblight.md
+RGBLIGHT_ENABLE = yes
+
+TAP_DANCE_ENABLE = no
+
+MOUSEKEY_ENABLE = yes
